0e9235c0c5d70ed3c176a9bb8a08cc399b845cbf
Henrique Santos <rickalmeida720@gmail.com>
2023-07-08 12:24:03 -0300

    project created

---
+++ src/main/java/com/example/blog/BlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApplication.class, args);
+	}
+
+}
